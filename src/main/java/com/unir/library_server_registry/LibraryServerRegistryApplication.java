e2e0c220d7847a3442616d8682c49e30c1054ad1
Alexander Cardenas <alexcardenas.u@gmail.com>
2023-01-23 17:46:56 -0500

    First commit

---
+++ src/main/java/com/unir/library_server_registry/LibraryServerRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.unir.library_server_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryServerRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryServerRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unir/library_server_registry/LibraryServerRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unir.library_server_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryServerRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
