38a7ce7dc300f8b431c14aa3d2f823095e46eaf8
Alexander Cardenas <alexcardenas.u@gmail.com>
2023-01-23 18:07:17 -0500

    config

--- src/main/java/com/unir/library_server_registry/LibraryServerRegistryApplication.java
+++ src/main/java/com/unir/library_server_registry/LibraryServerRegistryApplication.java
@@ -2,8 +2,10 @@ package com.unir.library_server_registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class LibraryServerRegistryApplication {
 
 	public static void main(String[] args) {
